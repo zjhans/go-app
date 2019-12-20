c0ebd7b10fd9072675e7f00b5e4fdd32ede68262
Zach Hanson <zachary_hanson@comcast.com>
2019-12-20 12:44:19 -0500

    Finished the basic Go web random number generator

---
+++ testApp.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+//Home function to serve a random number to the browser
+func Home(w http.ResponseWriter, r *http.Request) {
+	//rando := rand.Seed(time.Now().Unix())
+	welcome := "Golang, starting fresh. \nHave a random number:"
+	choiceNumber := rand.Intn(666)
+	numToStr := strconv.Itoa(choiceNumber)
+
+	w.Write([]byte(fmt.Sprintf(welcome)))
+	w.Write([]byte(fmt.Sprintf(numToStr)))
+}
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", Home)
+
+	http.ListenAndServe(":8080", mux)
+}
